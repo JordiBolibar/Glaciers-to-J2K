6d397cf427ebaf5186f8e28bc93f3d2d80bc9597
Jordi <jordib155@gmail.com>
2019-11-18 18:46:41 +0100

    Initial files

---
+++ glacier_hru_evolution.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: Jordi Bolibar
+Institut des Géosciences de l'Environnement (Université Grenoble Alpes)
+jordi.bolibar@univ-grenoble-alpes.fr
+
+DYNAMIC GLACIERIZED PERCENTAGE PER HRU PIXEL COMPUTATION
+J2K HYDROLOGICAL MODEL
+
+
+"""
