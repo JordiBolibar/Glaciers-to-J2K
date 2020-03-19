20850ece5508403474adc2e2091df34f3628c3cc
Jordi <jordib155@gmail.com>
2020-03-19 18:09:30 +0100

    Annual to daily resolution interpolation with Pandas
    The script has been optimized using Pandas. Glacierized fractions are now interpolated every year, only during the summer season (1/10/XXXX - 30/09/XXXX+1).

--- glacier_hru_evolution.py
+++ glacier_hru_evolution.py
@@ -15,6 +15,7 @@ import os
 #from numba import jit
 #from osgeo import gdal, ogr
 import pandas as pd
+#import datetime
 #import matplotlib.pyplot as plt
 #import matplotlib.backends.backend_pdf
 import numpy as np
@@ -40,45 +41,15 @@ hru_glacier_fractions_path = workspace + 'HRU_glacier_fractions\\'
 original_glacier_thickness_path = workspace + 'glacier_thickness\\original_glacier_thickness\\'
 aligned_glacier_thickness_path = workspace + 'glacier_thickness\\aligned_glacier_thickness\\'
 
+# List of paths to glacier ice thickness raster data
 list_path_glacier_thickness = np.asarray(os.listdir(original_glacier_thickness_path))
+
 
 ###############################################################################
 ###                           FUNCTIONS                                     ###
 ###############################################################################
 
-
-###############################################################################
-###                           MAIN                                          ###
-###############################################################################
-
-#import pdb; pdb.set_trace()
-
-#### Open HRU raster data ###
-raster_HRU = gdal.Open(hru_path + 'hru_cat.tif', gdal.GA_ReadOnly) 
-HRUs = raster_HRU.ReadAsArray()
-raster_HRU_landuse = gdal.Open(hru_path + 'hru_landuse.tif', gdal.GA_ReadOnly) 
-landuse_HRU = raster_HRU_landuse.ReadAsArray()
-
-# Land use #7 = ice
-HRU_ice_idx = np.where(landuse_HRU == 7)
-HRUs_ID_ice = np.unique(HRUs[HRU_ice_idx])
-
-raw_HRU_glacier_evolution = ['year']
-for HRU_ID in HRUs_ID_ice:
-    raw_HRU_glacier_evolution.append(int(HRU_ID))
-
-# Empty dataframe
-HRU_glacier_evolution_df = pd.DataFrame()
-
-# Iterate all years of glacier evolution data
-for path_glacier_thickness in list_path_glacier_thickness:
-   
-    full_path_glacier_thickness = original_glacier_thickness_path + path_glacier_thickness
-    print("\nGlacier ice thickness file: " + str(full_path_glacier_thickness))
-    
-    glacier_ID = path_glacier_thickness[-14:-9]
-    year = path_glacier_thickness[-8:-4]
-    
+def align_rasters(raster_HRU_landuse, full_path_glacier_thickness, aligned_glacier_thickness_path):
     #### Open raster data ###
     adfGeoTransform = raster_HRU_landuse.GetGeoTransform(can_return_null = True)
     
@@ -97,29 +68,126 @@ for path_glacier_thickness in list_path_glacier_thickness:
     raster_current_glacier = gdal.Open(vrt_glacier_thick_path, gdal.GA_ReadOnly) 
     current_glacier_thickness = raster_current_glacier.ReadAsArray()
     
+    return current_glacier_thickness
+
+def interpolate_glacier_fractions(hydro_year_range, hru_idx):
+    current_HRU_ice_fraction = []
+    for day in hydro_year_range:
+#                print(day)
+        if(day < (pd.Timestamp(int(year), 3, 1))):
+            # Same glacierized percentage during accumulation season
+            current_HRU_ice_fraction.append(float(raw_HRU_glacier_evolution[-1,hru_idx+1]))
+        else:
+            # Append the transition between the previous ice fraction and current year's
+            current_HRU_ice_fraction = np.concatenate((current_HRU_ice_fraction, np.linspace(current_HRU_ice_fraction[-1], glacierized_perc_HRU, num=(hydro_year_range[-1] - day).days+1)))
+            break
+        
+    return current_HRU_ice_fraction
+        
+def initialize_dataframe(i_interp_annual_ice_fraction, i_hydro_year_range):
+    print("\nInitializing dataframe")
+    start_joining = False
+    daily_HRU_glacier_evolution_df = pd.DataFrame()
+    for hru, HRU_ID  in zip(i_interp_annual_ice_fraction, HRUs_ID_ice):
+            current_column = pd.DataFrame(data={str(int(HRU_ID)): hru}, index = i_hydro_year_range)
+            if(start_joining):
+                daily_HRU_glacier_evolution_df = daily_HRU_glacier_evolution_df.join(current_column)
+            else:
+                daily_HRU_glacier_evolution_df = current_column
+                start_joining = True
+    
+    return daily_HRU_glacier_evolution_df
+
+
+###############################################################################
+###                           MAIN                                          ###
+###############################################################################
+
+
+#### Open HRU raster data ###
+raster_HRU = gdal.Open(hru_path + 'hru_cat.tif', gdal.GA_ReadOnly) 
+HRUs = raster_HRU.ReadAsArray()
+raster_HRU_landuse = gdal.Open(hru_path + 'hru_landuse.tif', gdal.GA_ReadOnly) 
+landuse_HRU = raster_HRU_landuse.ReadAsArray()
+
+# Land use #7 = ice
+HRU_ice_idx = np.where(landuse_HRU == 7)
+HRUs_ID_ice = np.unique(HRUs[HRU_ice_idx])
+
+raw_HRU_glacier_evolution = ['year']
+columns = ['date']
+for HRU_ID in HRUs_ID_ice:
+    raw_HRU_glacier_evolution.append(int(HRU_ID))
+    columns.append(str(int(HRU_ID)))    
+
+isFirst, initDF = True, True
+
+
+######## Iterate all years of glacier evolution data   ###########
+
+for path_glacier_thickness in list_path_glacier_thickness:
+   
+    full_path_glacier_thickness = original_glacier_thickness_path + path_glacier_thickness
+#    print("\nGlacier ice thickness file: " + str(full_path_glacier_thickness))
+    
+    glacier_ID = path_glacier_thickness[-14:-9]
+    year = path_glacier_thickness[-8:-4]
+    
+    # Align the current glacier ice thickness raster to the baseline HRU raster
+    current_glacier_thickness = align_rasters(raster_HRU_landuse, full_path_glacier_thickness, aligned_glacier_thickness_path)
+    
     ice_mask = np.where(current_glacier_thickness > 0)
     current_year = [year]
     
-    # We compute the glaciarized fraction for each HRU with ice
+    # We create the dates of the hydrological year with an annual timestep
+    year = int(year)
+    print("\nYear: " + str(year))
+    hydro_year_range = pd.date_range(start=str(year-1) + '-10-01', end=str(year) + '-09-30')
+    interp_annual_ice_fraction = []
+    
+    ########   We compute the glaciarized fraction for each HRU with ice   ###########
+    
+    hru_idx = 0
     for HRU_ID in HRUs_ID_ice:
-#        print("\nHRU ID: " + str(int(HRU_ID)))
         glacier_HRU_idx = np.where(HRUs == HRU_ID)
         current_thickness_HRU = current_glacier_thickness[glacier_HRU_idx]
         glacierized_perc_HRU = current_thickness_HRU[current_thickness_HRU > 0].size/HRUs[glacier_HRU_idx].size
         
         # We create a new row to be stacked on the raw data
         current_year.append(glacierized_perc_HRU)
-    
-    # We add the row for the current year to the raw data
+        
+        if(not isFirst):
+            # Move from annual to daily resolution
+            current_HRU_ice_fraction = interpolate_glacier_fractions(hydro_year_range, hru_idx)
+            
+            # We add the current HRU's interpolated data to all the data from this year
+            current_HRU_ice_fraction = np.asarray(current_HRU_ice_fraction)
+            interp_annual_ice_fraction.append(current_HRU_ice_fraction)
+            
+        hru_idx=hru_idx+1
+        
+    # We add the raw annual data to the matrix
     raw_HRU_glacier_evolution = np.vstack((raw_HRU_glacier_evolution, current_year))
+    
+    # We add the daily dataframe with the current year to the full daily dataframe
+    if(not isFirst):
+        interp_annual_ice_fraction = np.asarray(interp_annual_ice_fraction)
+        if(initDF):
+            daily_HRU_glacier_evolution_df = initialize_dataframe(interp_annual_ice_fraction, hydro_year_range)
+            initDF = False
+        else:
+            new_year_dataframe = initialize_dataframe(interp_annual_ice_fraction, hydro_year_range)
+            daily_HRU_glacier_evolution_df = daily_HRU_glacier_evolution_df.append(new_year_dataframe)
+    
+    isFirst = False
 
-# We add the raw data into the dataframe
+# We add the raw data into the original annual dataframe
 HRU_evolution_df = pd.DataFrame(index= raw_HRU_glacier_evolution[1:,0], columns=raw_HRU_glacier_evolution[0,1:], data=raw_HRU_glacier_evolution[1:,1:])
-print("\nProcessed glacierized HRUs evolution dataframe: " + str(HRU_evolution_df))
-#import pdb; pdb.set_trace()
- 
+
+print("\nProcessed glacierized HRUs daily evolution dataframe: " + str(daily_HRU_glacier_evolution_df))
+
 # Save output in file to be read by J2K
-HRU_evolution_df.to_csv(hru_glacier_fractions_path + "HRU_glacier_fractions_" + str(HRU_evolution_df.index.min()) + "_" + str(HRU_evolution_df.index.max()) + '.dat', sep=' ', index_label = 'year')
+daily_HRU_glacier_evolution_df.to_csv(hru_glacier_fractions_path + "HRU_glacier_fractions_" + str(HRU_evolution_df.index.min()) + "_" + str(HRU_evolution_df.index.max()) + '.dat', sep=' ', index_label = 'date')
    
     
     
